6acff50516bed85a2af9073ecdfa0479500b2e3f
zlf <2768551700@qq.com>
2022-11-24 10:17:22 +0800

    git-ssh commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -3,5 +3,6 @@ public class gitTest {
         System.out.println("git save...");
         System.out.println("git master save...");
         System.out.println("git push save...");
+        System.out.println("git-ssh push save...");
     }
 }
